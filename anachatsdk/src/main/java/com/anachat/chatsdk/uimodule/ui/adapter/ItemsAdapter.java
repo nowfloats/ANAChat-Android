8e575e2e13150f4d4924102a153b56b8487ee23f
rakshitsoni <rakshit.soni@nowfloats.com>
2018-02-26 16:29:17 +0530

    fix: minor ui fix

--- anachatsdk/src/main/java/com/anachat/chatsdk/uimodule/ui/adapter/ItemsAdapter.java
+++ anachatsdk/src/main/java/com/anachat/chatsdk/uimodule/ui/adapter/ItemsAdapter.java
@@ -62,7 +62,6 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.MyViewHolder
         this.imageLoader = imageLoader;
         this.message = message;
         this.enabled = enabled;
-        itemList.get(0).setDesc("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
         try {
             maxHeight = getMaxHeight(itemList, imageLoader.getContext());
         } catch (Exception e) {
